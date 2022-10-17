962d1a762fe77a4192949febcc159627d87c901b
Anit Srivastava <anit990@gmail.com>
2022-10-18 01:21:30 +0530

    index

--- DemoWebApplication/Program.cs
+++ DemoWebApplication/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.Azure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration["thismyconnectionstring:blob"], preferMsi: true);
+    clientBuilder.AddQueueServiceClient(builder.Configuration["thismyconnectionstring:queue"], preferMsi: true);
+});
 
 var app = builder.Build();
 
